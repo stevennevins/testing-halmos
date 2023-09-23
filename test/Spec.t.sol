c9a9fcdd51b3af24caaac97bb44734ece2d939cc
steven nevins <steven.nevins.js@gmail.com>
2023-09-23 11:14:05 -0400

    make symbolic test more like fuzz test

--- test/Spec.t.sol
+++ test/Spec.t.sol
@@ -18,18 +18,17 @@ contract SenderSpec is SymTest, Test {
         checkSender = address(new CheckSender());
     }
 
-    function check_caller() public {
-        address caller = svm.createAddress('caller');
+    function check_caller(bytes4 selector, address caller) public {
         /// comment out this line out, and halmos can find the address and the function selector 
         vm.assume(caller != OWNER);
 
+        bytes memory data = abi.encodeWithSelector(selector, "");
         vm.prank(caller);
-        bytes memory data = svm.createBytes(4, 'data');
         (bool success,) = address(checkSender).call(data);
-        assert(!success);
+        assertFalse(success);
     }
 
-    function test_caller(bytes4 selector, address caller) public {
+    function testFuzz_caller(bytes4 selector, address caller) public {
         /// comment out this line out. The fuzzer cant find the address and the function selector 
         vm.assume(caller != OWNER);
 
