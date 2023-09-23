927973359fcd3e2f4bd84fa8e50045e1d654b736
steven nevins <steven.nevins.js@gmail.com>
2023-09-23 10:09:23 -0400

    add fuzzer example

--- test/Spec.t.sol
+++ test/Spec.t.sol
@@ -22,9 +22,20 @@ contract SenderSpec is SymTest, Test {
         address caller = svm.createAddress('caller');
         /// comment out this line out, and halmos can find the address and the function selector 
         vm.assume(caller != OWNER);
+
         vm.prank(caller);
         bytes memory data = svm.createBytes(4, 'data');
         (bool success,) = address(checkSender).call(data);
         assert(!success);
+    }
+
+    function test_caller(bytes4 selector, address caller) public {
+        /// comment out this line out. The fuzzer cant find the address and the function selector 
+        vm.assume(caller != OWNER);
+
+        bytes memory data = abi.encodeWithSelector(selector, "");
+        vm.prank(caller);
+        (bool success,) = address(checkSender).call(data);
+        assertFalse(success);
     }
 }
